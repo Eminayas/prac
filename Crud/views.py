d824d20bf4adf9ae1c57bf6108ea995c9b2da95d
eminayas <eminayas@unemi.edu.ec>
2022-12-15 17:04:31 -0500

    turbio

--- Crud/views.py
+++ Crud/views.py
@@ -14,6 +14,28 @@ def Estudiante(request):
     return render(request, '_estudiante.html', {'form': form})
 
 
-def usuario(request):
-    return render(request, '_usuario.html')
 
+def usuario(request):
+
+    if request.method == 'POST':
+    
+        N_Identificacion = request.POST['txtid']
+        T_Identificacion = request.POST['txtidt']
+        Nombre = request.POST['txtnom']
+        Apellido_P = request.POST['txtapep']
+        Apellido_M = request.POST['txtapem']
+        Fecha_Nac = request.POST['txtfnca']
+        Usu_Django = request.POST['fnac']
+        Ciudad = request.POST['fnac']
+        Telf_Celular = request.POST['fnac']
+
+        usuario = usuario.objects.create(
+         N_Identificacion=N_Identificacion, T_Identificacion=T_Identificacion, Nombre=Nombre, Apellido_P=Apellido_P, Apellido_M=Apellido_M,
+         Fecha_Nac=Fecha_Nac, Usu_Django=Usu_Django, Pais=Pais, Provincia=Provincia, Ciudad=Ciudad, Telf_Celular=Telf_Celular)
+        messages.success(request, '¡Usuario registrado!')
+
+        return render(request, '_usuario.html')
+        
+    else:
+
+        return render(request, '_usuario.html')
